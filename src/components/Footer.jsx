31995a0693d39a0342c84150a98b987b66e48dbc
Nirav Joshi <nrvjj008@gmail.com>
2023-09-20 05:34:15 -0400

    library

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,12 +2,12 @@
 
 import Link from 'next/link';
 import {FaInstagram} from "react-icons/fa";
-import {FaTwitter} from "react-icons/fa6";
+import {FaEnvelope, FaTwitter} from "react-icons/fa6";
 export default function Footer() {
     const currentYear = new Date().getFullYear();
 
     return (
-        <div className="flex items-center justify-center space-x-2">
+        <div className="flex items-center justify-center space-x-2 px-4">
             © {currentYear} Nasaq Library All rights reserved.
 
             <Link href="https://instagram.com/nasaq_library?igshid=NGVhN2U2NjQ0Yg==">
@@ -18,6 +18,7 @@ export default function Footer() {
                     <FaTwitter  size={"2.5em"} className={"px-2"} />
 
             </Link>
+            <Link href={"mailto:info@nasaqlibrary.org"}><FaEnvelope size={"2.5em"} className={"px-2"}/></Link>
         </div>
     );
 }
